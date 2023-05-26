e1cb4d1dd90cc0abc756424f42c33ba2d04ae977
Shivansh10000 <shivanshj157@gmail.com>
2023-05-26 18:15:16 +0530

    adding info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
                     </p>
                 </div>
                 <p className='text-xl mt-5'>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci ea rem ipsum, nihil repellendus corrupti qui minima saepe voluptate dolorem, tempore unde quibusdam? Voluptate maiores inventore magnam rerum sunt eveniet accusantium deserunt repudiandae. Hic amet in doloribus? Ab possimus beatae modi, doloremque, accusantium provident labore reprehenderit placeat, ipsum qui aliquam.
+                My Name is Shivansh Joshi, and I am currently in the 4th year (2024 Gradute) of B.Tech in Information Technology(I.T) at Delhi Technological University (formerly DCE). I like to design web application using MERN stack.
                 </p>
 
                 <br />
@@ -21,7 +21,7 @@ const About = () => {
                     <div className="flex flex-col justify-center md:w-1/2 md:pr-4">
                         <div className='flex flex-col justify-center md:items-center'>
                             <p className='text-xl mt-5'>Delhi Technological University</p>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum dignissimos, numquam repellendus exercitationem et vero cum tempora similique cumque nobis!</p>
+                            <p>Currently studying IT in Delhi Technlogical University, enthusiatic about competitive Programming, Coding and Web Development. <br /> Aggregate CGPA : 9.01</p>
                         </div>
                         <div className='flex justify-center items-center'>
                             <img src={college} alt="" width={300} height={100} />
@@ -30,7 +30,7 @@ const About = () => {
                     <div className="flex flex-col justify-center md:w-1/2 md:pl-4">
                         <div className='flex flex-col justify-center md:items-center'>
                             <p className='text-xl mt-5'>VBPS</p>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum dignissimos, numquam repellendus exercitationem et vero cum tempora similique cumque nobis!</p>
+                            <p>Completed High School in 2019 <br /> 10th CGPA : 9.60 <br /> 12th Percentage : 92%</p>
                         </div>
                         <div className='flex justify-center items-center'>
                             <img src={school} alt="" width={300} height={100} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,14 +10,12 @@ const Home = () => {
       className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full">
+        <div className="flex flex-col justify-center h-full max-w-lg text-center md:text-left">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
             I'm a Full Stack Developer
           </h2>
-          <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+          <p className="text-gray-500 py-4">
+            I like to design and build software. Currently, I love to work on web applications using technologies like React, Tailwind, Node, and Express.
           </p>
 
           <div>
@@ -35,11 +33,11 @@ const Home = () => {
           </div>
         </div>
 
-        <div>
+        <div className="flex justify-center md:justify-start">
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="rounded-2xl mx-auto w-2/3 md:w-full max-w-xs md:max-w-none"
           />
         </div>
       </div>
